35fc2406b57b0566f9534c2283c812b13077a7ff
Jackbobio <wickenbergjakob@gmail.com>
2025-05-04 23:09:21 +0200

    Fix import path for checkAdminRole middleware in admin route

--- klubbsida-server/routes/admin.js
+++ klubbsida-server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { checkJwt } = require('../middleware/jwtCheck');
-const { checkAdminRole } = require('../middleware/checkAdminRole');
+const { checkAdminRole } = require('../middleware/roleCheck');
 
 router.get('/', checkJwt, checkAdminRole, (req, res) => {
     res.json({ message: 'Hello from the backend!' });
