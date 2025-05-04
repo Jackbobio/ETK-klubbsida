2896eb8e9980ac810227ec16ab192129db5afb90
Jackbobio <wickenbergjakob@gmail.com>
2025-05-04 23:15:10 +0200

    Implement checkAdminRole function directly in admin route for role validation

--- klubbsida-server/routes/admin.js
+++ klubbsida-server/routes/admin.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { checkJwt } = require('../middleware/jwtCheck');
-const { checkAdminRole } = require('../middleware/roleCheck');
+
+function checkAdminRole(req, res, next) {
+    const roles = req.user["https://localhost:5173/roles"] || [];
+    if (roles.includes("Administrator")) {
+        next();
+    } else{
+        return res.status(403).send("Access denied. Admin role required.");
+    }
+    
+}
 
 router.get('/', checkJwt, checkAdminRole, (req, res) => {
     res.json({ message: 'Hello from the backend!' });
