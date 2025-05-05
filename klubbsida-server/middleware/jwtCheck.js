d886ccb0815abdb3baa7d7167717ce3d187294c4
Jackbobio <wickenbergjakob@gmail.com>
2025-05-05 21:01:06 +0200

    Refactor JWT authentication middleware to use express-oauth2-jwt-bearer and update package dependencies

--- klubbsida-server/middleware/jwtCheck.js
+++ klubbsida-server/middleware/jwtCheck.js
@@ -1,5 +1,6 @@
-const { expressjwt: jwt } = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+// const { expressjwt: jwt } = require('express-jwt');
+// const { jwt } = require('jwks-rsa');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 /**
  * JWT authentication middleware
@@ -8,15 +9,23 @@ const jwksRsa = require('jwks-rsa');
  */
 
 
-const jwtCheck = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksUri: 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/.well-known/jwks.json'
-  }),
-  audience: ['klubbsida.onrender.com/api', 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/userinfo'], // <-- this should match ONE of the aud fields
-  issuer: 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/',
-  algorithms: ['RS256'],
+
+const jwtCheck = auth({
+  audience: 'klubbsida.onrender.com/api',
+  issuerBaseURL: 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/',
+  tokenSigningAlg: 'RS256'
 });
+
+
+// const jwtCheck = jwt({
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksUri: 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/.well-known/jwks.json'
+//   }),
+//   audience: ['klubbsida.onrender.com/api', 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/userinfo'], // <-- this should match ONE of the aud fields
+//   issuer: 'https://dev-nwurgok5vi3aouh3.eu.auth0.com/',
+//   algorithms: ['RS256'],
+// });
 
 module.exports = jwtCheck;  
