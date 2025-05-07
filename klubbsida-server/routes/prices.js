19abbf5902b812b719803955c12ffa38b258bc8b
Jackbobio <wickenbergjakob@gmail.com>
2025-05-07 18:58:38 +0200

    Refactor prices route to return all prices instead of a specific item; update error message for better clarity

--- klubbsida-server/routes/prices.js
+++ klubbsida-server/routes/prices.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Price = require('./models/price');
 
-router.get('/:item', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const prices = await Price.findOne({ item: req.params.item });
+        const prices = await Price.find();
         if (prices.length === 0) {
-            return res.status(404).json({ message: 'No prices found for this item' });
+            return res.status(404).json({ message: 'No prices found for' });
         }
         res.json(prices);
     } catch (err) {
