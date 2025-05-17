39e946f6bdad971056d7a3c7ff0dde854bcdd88d
Jacke0877 <jakob.wickenberg@student.tabyenskilda.se>
2025-05-17 11:32:17 +0200

    fix: update redirect_uri in Auth0Provider to use dynamic URL based on environment

--- klubbsida-klient/src/main.jsx
+++ klubbsida-klient/src/main.jsx
@@ -5,14 +5,19 @@ import './index.css'
 import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+const isLocalhost = window.location.origin === "http://localhost:5173";
+const redirectUri = isLocalhost
+  ? "http://localhost:5173"
+  : "https://madebyjakob.github.io/ETK-klubbsida";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: "klubbsida.onrender.com/api",    
+        redirect_uri: redirectUri,
+        audience: "klubbsida.onrender.com/api",
       }}
     >
       <App />
